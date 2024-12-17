0402e3bce30821b898e5006e3288be82fa502a95
muunimoz <160251803+muunimoz@users.noreply.github.com>
2024-12-17 16:28:09 +0000

    test

---
+++ Test.java
@@ -0,0 +1,10 @@
+public class Test {
+  public static void main(String[] args) { 
+    System.out.println("31歳です");
+    System.out.println("31 + 31 の計算をします");
+    System.out.println(31 + 31);
+    int x;
+    x = 6;
+    System.out.println(x * x * 3.14);
+  }
+}
